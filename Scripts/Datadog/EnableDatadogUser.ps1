f4e4c096c05c86807e881eef86b75f2dc65fa5d8
denny@zohno.com <denny@zohno.com>
2017-01-26 20:40:56 +0530

    Enable datadog user

---
+++ Scripts/Datadog/EnableDatadogUser.ps1
@@ -0,0 +1,6 @@
+﻿$ApiKey = 'Api key is here'
+$AppKey = 'App key is here'
+$UserEmail = args[0]
+
+$Result = Invoke-WebRequest -Uri ("https://app.datadoghq.com/api/v1/user/" + $UserEmail + "?api_key=" + $ApiKey + "&application_key=" + $AppKey+ ""| echo) -body '{"disabled": false}' -ContentType "application/json" -Method PUT
+$Result.Content
