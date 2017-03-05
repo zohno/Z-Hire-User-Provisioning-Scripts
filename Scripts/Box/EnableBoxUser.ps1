269bc5dcfc5f1040c1759f429bb45688bb33f9e1
denny@zohno.com <denny@zohno.com>
2017-03-05 14:42:17 +0530

    Box script

---
+++ Scripts/Box/EnableBoxUser.ps1
@@ -0,0 +1,10 @@
+﻿$UserName = args[0]
+$Token = 'Bearer PasteTokenHere'
+
+$Headers = @{
+    Authorization = $Token
+}
+$results = Invoke-RestMethod -Uri ("https://api.box.com/2.0/users") -Headers $headers -Method GET
+$Id = ($results.entries | Select-Object -Property name, status,id | Where-Object {$_.name -eq $UserName}).id
+
+Invoke-RestMethod -Uri ("https://api.box.com/2.0/users/"+ $Id + "" | echo) -body '{"status": "active"}' -Headers $headers -Method PUT
